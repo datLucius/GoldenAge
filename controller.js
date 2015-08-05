0712aca627b51ac4475b38dc9298383e74ab047e
datLucius <jlnelsonart@gmail.com>
2015-08-05 02:00:59 -0400

    fonts

--- controller.js
+++ controller.js
@@ -78,18 +78,6 @@
       $('.detailView').removeClass('hideThis');
     };
  })
- .directive('ngEnter', function () {
-    return function (scope, element, attrs) {
-        element.bind("keydown keypress", function (event) {
-            if(event.which === 38) {
-                scope.$apply(function (){
-                    graphicWidth = graphicWidth + 10;
-                });
 
-                event.preventDefault();
-            }
-        });
-    };
-});
 
 })();
